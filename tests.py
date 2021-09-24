03eaa4d9d61fc4aef250eeb4fc4ab9c51abab592
Izentall <73279540+Izentall@users.noreply.github.com>
2021-09-24 17:57:00 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -31,3 +31,30 @@ def test_tuple_index(a, b, c, d, value):
         assert tup.index(value)
     except ValueError:
         pass
+
+
+@pytest.mark.parametrize('length', [0, 1, 10])
+def test_set_len(length):
+    s = set()
+    for i in range(length):
+        s.add(i)
+    assert len(s) == length
+
+
+def test_set_union():
+    s1 = set([0, 2, 4, 6, 8, 10, 12])
+    s2 = set([0, 3, 6, 9, 12])
+    check = set([0, 2, 3, 4, 6, 8, 9, 10, 12])
+    assert s1.union(s2) == check
+
+
+@pytest.mark.parametrize('s, elem',
+                         [
+                             ([1, 2, 3], 4),
+                             ([1, 2, 3, 4], 4),
+                             ([1, 2, 3, 4, 4], 4)
+                         ])
+def test_set_discard(s, elem):
+    s1 = set(s)
+    s1.discard(elem)
+    assert not (elem in s1)
