b3eaf3895a43b4b9fd4ab25d5cfc51a9f7e099b0
Izentall <73279540+Izentall@users.noreply.github.com>
2021-09-24 17:32:49 +0300

    Add tests for tuple

--- tests.py
+++ tests.py
@@ -1,1 +1,33 @@
+import pytest
 
+
+def test_tuple_order():
+    (a, b) = (1, 2)
+    assert a == 1
+    assert b == 2
+
+
+@pytest.mark.parametrize('a, b, c, d, value, result',
+                         [
+                             (1, 2, 3, 4, 0, 0),
+                             (0, 0, 0, 0, 0, 4),
+                             (1, 2, 3, 4, 4, 1),
+                             (-1, -2, -3, -1, -1, 2),
+                             (0, 3, 3, 3, 3, 3)
+                         ])
+def test_tuple_count(a, b, c, d, value, result):
+    tup = (a, b, c, d)
+    assert tup.count(value) == result
+
+
+@pytest.mark.parametrize('a, b, c, d, value',
+                         [
+                             (1, 2, 3, 4, 0),
+                             (0, 0, 0, 0, -1)
+                         ])
+def test_tuple_index(a, b, c, d, value):
+    tup = (a, b, c, d)
+    try:
+        assert tup.index(value)
+    except ValueError:
+        pass
